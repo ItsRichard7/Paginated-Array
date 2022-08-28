2961f2118ff67c050c48d8cc683a727088faa51f
ItsRichard7 <ricardobm@estudiantec.cr>
2022-08-28 15:19:59 -0600

    Create a function that takes the numbers of the text file and convert it in a binary file

--- FileCreation.cpp
+++ FileCreation.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <string>
 #include "FileCreation.h"
-#include "fstream"
+#include <fstream>
+#include <sstream>
 
 using namespace std;
 
@@ -10,7 +11,7 @@ using namespace std;
 int FileCreation::fillNumbers(string filename, int Kilobytes){
     filename += ".csv";
     string filePath = "/home/itsrichard/Proyectos C++/Tarea Extraclase 1/Paginated-Array/TestFiles/" + filename;
-    int numLimit = (256/2) * Kilobytes; // Used (256 / 2) ints because we need to save the number and also their direccion in the array so for create an array with 1 kb of memory used divide the number for 2
+    int numLimit = (256) * Kilobytes; // Used (256 / 2) ints because we need to save the number and also their direccion in the array so for create an array with 1 kb of memory used divide the number for 2
     int tmp;
     string binaryNum;
     fstream writeFile;
@@ -27,9 +28,26 @@ int FileCreation::fillNumbers(string filename, int Kilobytes){
 }
 
 int FileCreation::convertIntFile(string filename) {
+    string pathIntFile = "/home/itsrichard/Proyectos C++/Tarea Extraclase 1/Paginated-Array/TestFiles/" + filename;
     filename.erase(filename.end()-4, filename.end());
-    string newfile = filename + "Binary.csv";
+    string newfile = filename + "Binary.bin";
+    string pathBinaryFile = "/home/itsrichard/Proyectos C++/Tarea Extraclase 1/Paginated-Array/FilesBinary/" + newfile;
     cout << newfile << endl;
+    string line, word;
+    fstream intFile(pathIntFile, ios::in);
+    ofstream binaryFile;
+    binaryFile.open(pathBinaryFile, ios::binary);
+    if (intFile.is_open()){
+        while (getline(intFile, line)) {
+            stringstream str(line);
+            while (getline(str, word, ',')) {
+                cout << word << " ";
+                int num = stoi(word);
+                binaryFile.write(reinterpret_cast<const char *>(&num), sizeof(num));
+            }
+        }
+    }
+    binaryFile.close();
     return 0;
 }
 
--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include "FileCreation.h"
 #include "SortAlgorithms.h"
+#include "BinaryTraductor.h"
 
 using namespace std;
 
 int main(int args, char** argv) {
     FileCreation fileCreator;
-
+    fileCreator.convertIntFile("1KB.csv");
     return 0;
 }
